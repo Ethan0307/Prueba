6b139c5ff9c5d6c706e68820001a9f4fe0eb4fe1
Ethan0307 <sanchezethan409@gmail.com>
2020-05-16 14:53:35 -0500

    Hice un hola mundo

---
+++ Prueba_Git/src/paquete1/Prueba.java
@@ -0,0 +1,22 @@
+
+package paquete1;
+
+import javax.swing.JOptionPane;
+
+
+public class Prueba {
+    
+    
+    public static void main(String[] args) {
+        
+        
+        JOptionPane.showMessageDialog(null, "Hola mundo");
+        
+        
+    }
+    
+    
+    
+    
+    
+}
